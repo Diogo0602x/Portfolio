e101da14ebbd89b8ac32f16019bc89fc41a1e7d9
Diogo0602x <diogoteixeira0602@gmail.com>
2021-07-21 22:24:05 -0300

    Orçamento feito

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function rangeSlide(value){
+  document.getElementById('rangeValue').innerHTML = value
+}
+
 document.querySelector(".lines").addEventListener("click", () =>
   document.querySelector(".container").classList.toggle("show-menu")
 );
