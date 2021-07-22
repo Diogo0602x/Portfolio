e86f5c0b453ea699d09dd089666da7b2cd682845
Diogo0602x <diogoteixeira0602@gmail.com>
2021-07-22 09:54:01 -0300

    Javascript terminado

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function rangeSlide(value){
 document.querySelector(".lines").addEventListener("click", () =>
   document.querySelector(".container").classList.toggle("show-menu")
 );
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+  
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+   
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
